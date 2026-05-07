fc3849b46420d6737dc8239caa82aa7e810327c7
thilina-fer <dilshanthilina1010@gmail.com>
2026-05-07 16:04:11 +0530

    feat: Implement addition function and update main module to demonstrate usage

---
+++ Modules/Main.py
@@ -0,0 +1,15 @@
+# from Addition import add
+# print(f"Addition file output : {add(5, 3)}")
+
+# OR
+
+# import Addition
+
+# rsult = Addition.add(5, 3)
+# print(f"Addition file output : {rsult}")
+
+# OR
+
+import Addition as ad
+result = ad.add(5, 3)
+print(f"Addition file output : {result}")
