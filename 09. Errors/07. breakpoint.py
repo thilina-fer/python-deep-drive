a7c78e2cd2de1cc52832f724e6b937e941308fa5
thilina-fer <dilshanthilina1010@gmail.com>
2026-06-11 16:53:12 +0530

    feat: Add multiplication function with user input and breakpoint for debugging

---
+++ 09. Errors/07. breakpoint.py
@@ -0,0 +1,9 @@
+def multiplication(a, b):
+    answer = a * b
+    return answer
+
+breakpoint()
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+mul = multiplication(x, y)
+print(mul)
